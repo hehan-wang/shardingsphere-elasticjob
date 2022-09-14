45f772124d714098b2d885480971caefa3e68465
wanghehan <wanghehan@xiaomi.com>
2022-09-14 16:53:22 +0800

    push

--- elastic-job-lite/build.sh
+++
@@ -1,7 +0,0 @@
-# build
-docker build -t elastic-job-lite-console:2.1.5 .
-
-# run
-#docker run -d -p 8899:8899 elastic-job-lite-console:2.1.5
-
-#push
---
+++ elastic-job-lite/elastic-job-lite-console/build.sh
@@ -0,0 +1,9 @@
+# build
+docker build -t elastic-job-lite-console:2.1.5 .
+
+# run
+#docker run -d -p 8899:8899 elastic-job-lite-console:2.1.5
+
+#push
+docker tag elastic-job-lite-console:2.1.5 micr.cloud.mioffice.cn/elastic-job/elastic-job-lite-console:2.1.5
+docker push micr.cloud.mioffice.cn/elastic-job/elastic-job-lite-console:2.1.5
