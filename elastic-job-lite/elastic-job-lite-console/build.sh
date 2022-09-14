25451a069e63d8b284960163e2245db2b0ca9cb0
wanghehan <wanghehan@xiaomi.com>
2022-09-14 17:22:18 +0800

    fix

--- elastic-job-lite/elastic-job-lite-console/build.sh
+++ elastic-job-lite/elastic-job-lite-console/build.sh
@@ -2,7 +2,7 @@
 docker build -t elastic-job-lite-console:2.1.5 .
 
 # run
-#docker run -d -p 8899:8899 elastic-job-lite-console:2.1.5
+#docker run -d -p 8080:8899 elastic-job-lite-console:2.1.5
 
 #push
 docker tag elastic-job-lite-console:2.1.5 micr.cloud.mioffice.cn/elastic-job/elastic-job-lite-console:2.1.5
